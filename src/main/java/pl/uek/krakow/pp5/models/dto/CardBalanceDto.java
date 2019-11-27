5202adae96c865ecea7e81f18de698fbd79242cb
jacekku <kecajwy@gmail.com>
2019-11-27 21:27:53 +0100

    adding endpoint tests

--- src/main/java/pl/uek/krakow/pp5/models/dto/CardBalanceDto.java
+++ src/main/java/pl/uek/krakow/pp5/models/dto/CardBalanceDto.java
@@ -2,11 +2,13 @@ package pl.uek.krakow.pp5.models.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CardBalanceDto {
 	public String cardNumber;
 	public BigDecimal balance;
---
+++ src/test/java/pl/uek/krakow/pp5/models/e2e/EndpointsTest.java
@@ -0,0 +1,37 @@
+package pl.uek.krakow.pp5.models.e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.uek.krakow.pp5.models.CreditCardFacade;
+import pl.uek.krakow.pp5.models.dto.CardBalanceDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class EndpointsTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	TestRestTemplate http;
+
+
+	@Test
+	public void itListsCardBalances() {
+		String uri = "/api/cards/balances";
+		String url = String.format("http://localhost:%s%s", port, uri);
+
+		ResponseEntity response = http.getForEntity(url, CardBalanceDto[].class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+}
